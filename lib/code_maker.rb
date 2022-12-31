41e37fe5d0d745cf21df1287a9c9c58a033973a7
dna737 <adnanhere27@gmail.com>
2022-12-31 13:11:07 +0530

    clear screen after code is created

--- lib/code_maker.rb
+++ lib/code_maker.rb
@@ -9,9 +9,11 @@ class CodeMaker
     @colors = [].push('V').push('I').push('B').push('G').push('Y').push('O')
     if number == 1 
       human     #The user makes the code, and the computer has to break it.
+      print "\e[2J\e[H"
       @code_breaker = CodeBreaker.new(1)
     else
       computer  #The computer makes the code, and the human has to break it.
+      print "\e[2J\e[H"
       @code_breaker = CodeBreaker.new(2)
     end
   end
