351e7b390202afed43908e51ef195c44803c6c19
dna737 <adnanhere27@gmail.com>
2022-12-31 13:09:20 +0530

    commit code_breaker and main.rb

--- lib/code_breaker.rb
+++ lib/code_breaker.rb
@@ -1,14 +1,31 @@
 class CodeBreaker
     @whites #colors that result in whites are stored in this hash.
     @blacks #colors that result in blacks are stored in this hash.
-    def initialize(number)
-      @whites = {}
-      @blacks = {}
-    end
+  def initialize(number)
+      if number == 1
+        #The computer needs to break the code.
+        @whites = {}
+        @blacks = {}
+        computer
+      else 
+        #The user needs to break the code. 
+        human 
+      end
+  end
 
+  def computer
     for i in 1..12 #you have 12 rounds to figure this out
 
     end
+  end 
 
+  def human
+    puts "\n There is a code."
+  end
+
+  #Lets us know the number of black or white pegs while breaking the code.
+  def verdict  
+
+  end
 
 end
--- lib/main.rb
+++ lib/main.rb
@@ -4,6 +4,6 @@ puts "Hello, and welcome to Mastermind!\n Please enter \"1\" to be a code breake
 
 input = gets.chomp.to_i
 until input == 1 or input == 2 
-  puts "Please enter \"1\" to be a code breaker, or \"2\" to be a code breaker."
+  puts "Please enter \"1\" to be a code maker, or \"2\" to be a code breaker."
 end
 
