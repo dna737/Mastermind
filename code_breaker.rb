e4a30ddc8c49fea3334c6cf08167fa64aa67a4a0
dna737 <adnanhere27@gmail.com>
2022-12-31 12:36:59 +0530

    rename file

---
+++ code_breaker.rb
@@ -0,0 +1,14 @@
+class CodeBreaker
+    @whites #colors that result in whites are stored in this hash.
+    @blacks #colors that result in blacks are stored in this hash.
+    def initialize
+      @whites = {}
+      @blacks = {}
+    end
+
+    for i in 1..12 #you have 12 rounds to figure this out
+
+    end
+
+
+end
