327572481cfb7dfb4211e2c4dba0623e5360958d
dna737 <adnanhere27@gmail.com>
2022-12-31 12:59:46 +0530

    make the CLI asking for input

--- lib/main.rb
+++ lib/main.rb
@@ -1,9 +1,11 @@
-require '../'
+require_relative './code_breaker.rb'
+require_relative './code_maker.rb'
 
 puts "Hello, and welcome to Mastermind!\n Please enter \"1\" to be a code breaker, or \"2\" to be a code breaker."
 
 input = gets.chomp.to_i
 until input == 1 or input == 2 
-  puts "Please enter \"1\" to be a code breaker, or \"2\" to be a code breaker."
+  puts "Please enter \"1\" to be a code maker, or \"2\" to be a code breaker."
 end
 
+CodeMaker.new(input)
