fb4267a3d0b0cb5b93f5d5c67149b0359dd3dfd3
dna737 <adnanhere27@gmail.com>
2022-12-31 12:29:16 +0530

    implement CodeMaker#human

--- code_maker.rb
+++ code_maker.rb
@@ -1,19 +1,24 @@
 # The Code maker comes up with a code that the opponent has to break.
 class CodeMaker
-  
+  private attr_accessor :code, :colors
   def initialize(number)
+    @code = []
+    @colors = [].push('V').push('I').push('B').push('G').push('Y').push('O')
     if number == 1 #The computer has to make the code.
       computer  
     else
-      human  
+      human #The user makes the code.
     end
-    @code = []
-    @colors = [].push("V").push("I").push("B").push("G").push("Y").push("O")
   end
 
+  private 
   #If the computer has to make the code:
   def computer
-
+    puts "The computer is generating the code..."
+    sleep (2)
+    until code.size == 4
+      code.push
+    end
   end
 
   #If the user has to make the code:
@@ -25,17 +30,21 @@ class CodeMaker
     B - Blue
     G - Green
     Y - Yellow
-    O - Orange"
+    O - Orange\n"
     
     #Storing 4 inputs:
-    until @code.size == 4
-      
+    until code.size == 4
+      puts "Please enter a letter. #{4 - code.size} more left"
+      input = gets.chomp[0]
+      if colors.include?(input.upcase)
+        code.push(input)
+      else
+        puts "Invalid input. Please enter a valid color."
+      end
     end
+    puts "Input entered!"
   end
 
-  private
-  @code  
-  @colors
 end
 
 CodeMaker.new(2)
