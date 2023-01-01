69b7b2d160e7806ccee2732e8cc2a11402871787
dna737 <adnanhere27@gmail.com>
2023-01-01 11:45:27 +0530

    replace first "breaker" with "maker"

--- lib/main.rb
+++ lib/main.rb
@@ -1,7 +1,7 @@
 require_relative './code_breaker.rb'
 require_relative './code_maker.rb'
 
-puts "Hello, and welcome to Mastermind!\n Please enter \"1\" to be a code breaker, or \"2\" to be a code breaker."
+puts "Hello, and welcome to Mastermind!\n Please enter \"1\" to be a code maker, or \"2\" to be a code breaker."
 
 input = gets.chomp.to_i
 until input == 1 or input == 2 
