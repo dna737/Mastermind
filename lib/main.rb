69526da1ec1a05714b057f4c128447027db0f203
dna737 <adnanhere27@gmail.com>
2023-01-03 13:51:03 +0530

    ask for input in "until" loop

--- lib/main.rb
+++ lib/main.rb
@@ -6,6 +6,7 @@ puts "Hello, and welcome to Mastermind!\n Please enter \"1\" to be a code maker,
 input = gets.chomp.to_i
 until input == 1 or input == 2 
   puts "Please enter \"1\" to be a code maker, or \"2\" to be a code breaker."
+  input = gets.chomp.to_i
 end
 
 CodeMaker.new(input)
