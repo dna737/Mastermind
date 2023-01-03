a68a1dee80d5bf3629c0378dc48c84a0b1d7bd57
dna737 <adnanhere27@gmail.com>
2023-01-03 13:44:36 +0530

    implement #computer; complete #verdict

--- lib/code_breaker.rb
+++ lib/code_breaker.rb
@@ -1,13 +1,14 @@
 class CodeBreaker
-  private attr_accessor :user_input, :whites, :blacks
-  private attr_reader :code, :colors
+  private attr_accessor :user_input, :guaranteed, :computer_result
+  private attr_reader :code, :colors, :number
   def initialize(number, code)
+    @number = number
     @code = code
     @colors = [].push('V').push('I').push('B').push('G').push('Y').push('O')
       if number == 1
         #The computer needs to break the code.
-        @whites = {}
-        @blacks = {}
+        @guaranteed = {}
+        @computer_result = []
         computer
       else 
         #The user needs to break the code.
@@ -15,15 +16,53 @@ class CodeBreaker
       end
   end
 
-  def initialize(input, code, something)
-    #This is for testing purposes only:
-    @user_input = []
-    verdict(input, code)
+
+  def computer   
+
+    i = 0
+
+    while i < 4
+    curr_color = colors[rand(colors.size)]
+    colors.delete(curr_color)
+    comp_input = computer_helper
+    if guaranteed != nil && !guaranteed.key?(curr_color)
+      for j in 0..3
+        remove_this = guaranteed.values
+        unless guaranteed.values.flatten.include?(j)
+          comp_input[j] = curr_color 
+        end
+      end
+    end
+    i += 1
+    if verdict(comp_input, code)
+      puts "The computer guessed your code in #{i} move(s)!"
+      puts "The computer's correct guess was: #{comp_input}"
+      break
+    else 
+      puts "The computer guessed it wrong. It has #{3-i} move(s) left!"
+      next
+    end
+    end
+  end
+  
+  #This method fills in the values that previously generated black pegs.
+  def computer_helper
+    #new array of size 4. Only insert in indices that are stored as values in the hash.
+    computer_result = Array.new(4)
+    unless guaranteed.empty?
+      #iterate through the hash and add the elements where you need to.
+      guaranteed.each do |char, indices| 
+        #the index is an array of indices. Iterate through the `index` array now.
+        unless indices.empty?
+          for index in indices
+          computer_result[index] = char
+          end 
+        end
+      end
+    end
+    computer_result
   end
 
-  def computer
-
-  end 
 
   def human
     puts "\n There is a code. Guess it in 12 (or less) moves to win!"
@@ -60,18 +99,28 @@ class CodeBreaker
   #Lets us know the number of black or white pegs when breaking the code.
   def verdict(input, code)  
     #Check the number of black pegs:
+    input_clone = input.clone
     code_clone = code.clone
     black_pegs = 0
     for j in 0...input.size
       if input != nil && input[j] != nil && input[j].is_a?(String) && input[j].upcase == code[j]  
         black_pegs += 1
-        input[j] = "0".to_i
+        if number == 1 #the computer's trying to break the code.
+          if guaranteed.key?(input[j])
+            unless guaranteed.values.flatten.include?(j)
+              guaranteed[input[j]].push(j)
+            end
+          else
+            guaranteed[input[j]] = [].push(j)
+          end
+        end
+        input_clone[j] = "0".to_i
         code_clone[j] = 0
       end
+
     end
     
     puts "Number of black pegs: #{black_pegs}"
-    puts "the code is #{code}"
     if black_pegs == 4 
       return true 
     end
@@ -83,7 +132,7 @@ class CodeBreaker
       if input[k].is_a?(String) && code_clone.include?(input[k].upcase)
         white_pegs += 1
         code_clone[code_clone.find_index(input[k].upcase)] = 0
-        input[k] = 0 
+        input_clone[k] = 0 
       end
     end
     puts "Number of white pegs: #{white_pegs}"
